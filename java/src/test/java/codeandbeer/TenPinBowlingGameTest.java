b530b41469f4d3966763030e03b2543579781876
Andrew Haines <andrew@haines.org.nz>
2016-02-25 11:01:35 +0000

    Add java skeleton

---
+++ java/src/test/java/codeandbeer/TenPinBowlingGameTest.java
@@ -0,0 +1,13 @@
+package codeandbeer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+
+public class TenPinBowlingGameTest {
+    @Test
+    public void shouldScoreZeroForAGameOfGutterBalls() {
+        fail();
+    }
+}
